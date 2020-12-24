f45ab97b549128e1d5ea2b3c59441be5f866867b
S. Zahit ACAR <zahitacar@yandex.com>
2020-12-24 08:07:59 +0000

    Güncel

--- script.js
+++ script.js
@@ -1,54 +1,77 @@
 const data = {
-  USD: {EUR: 0.82, GBP: 0.74},
-  EUR: {USD: 1.23, GBP: 0.91},
-  GBP: {USD: 1.35, EUR: 1.10},
-};
-
-const currencyKeys = Object.keys(data);
-
-function createCurrencyElements(elements, root, inputName){
-  for(let i =0; i< elements.length; i++){
-    const currencyKeyDiv   = document.createElement("div");
-    const currencyKeyInput = document.createElement("input");
-    currencyKeyInput.setAttribute("type", "radio");
-    currencyKeyInput.setAttribute("name", inputName);
-    currencyKeyInput.setAttribute("id", inputName + elements[i]);
-    currencyKeyInput.setAttribute("value", elements[i]);
-
-    const currencyKeyLabel = document.createElement("label");
-    currencyKeyLabel.setAttribute("for", inputName + elements[i]);
-    currencyKeyLabel.textContent = elements[i];
-
-    currencyKeyDiv.appendChild(currencyKeyInput);
-    currencyKeyDiv.appendChild(currencyKeyLabel);
-    root.appendChild(currencyKeyDiv);
+    USD: {EUR: 0.82, GBP: 0.74, TL: 7.68},
+    EUR: {USD: 1.23, GBP: 0.91, TL: 9.35},
+    GBP: {USD: 1.35, EUR: 1.10, TL: 10.21},
+    TL : {USD: 0.13, EUR: 0.11, GBP: 0.098}
+  };
+  
+  const currencyKeys = Object.keys(data);
+  
+  function createCurrencyElements(elements, root, inputName){
+    for(let i =0; i< elements.length; i++){
+      const currencyKeyDiv   = document.createElement("div");
+      const currencyKeyInput = document.createElement("input");
+      currencyKeyInput.setAttribute("type", "radio");
+      currencyKeyInput.setAttribute("name", inputName);
+      currencyKeyInput.setAttribute("id", inputName + elements[i]);
+      currencyKeyInput.setAttribute("value", elements[i]);
+  
+      const currencyKeyLabel = document.createElement("label");
+      currencyKeyLabel.setAttribute("for", inputName + elements[i]);
+      currencyKeyLabel.textContent = elements[i];
+  
+      currencyKeyDiv.appendChild(currencyKeyInput);
+      currencyKeyDiv.appendChild(currencyKeyLabel);
+      root.appendChild(currencyKeyDiv);
+    }
   }
-}
+  
+  //from
+  const parentEl = document.querySelector("#currency-box-from");
+  const fromInputName = "currency_from";
+  createCurrencyElements(currencyKeys, parentEl, fromInputName);
+  
+  // to
+  const parentToEl = document.querySelector("#currency-box-to");
+  const toInputName = "currency_to";
+  createCurrencyElements(currencyKeys, parentToEl, toInputName);
+  
+  
+  const calculateButton = document.querySelector("#calculate-button");
+  calculateButton.addEventListener("click", function(){
+    const currencyResult = document.querySelector("#currency-result");
+    
+   if (document.querySelector("input[name='currency_from']:checked") && document.querySelector("input[name='currency_to']:checked")){
+    
+    const fromTarget = document.querySelector("input[name='currency_from']:checked").value;
+        
+    const toTarget   = document.querySelector("input[name='currency_to']:checked").value;
+        
+    const amount     = document.querySelector("input[name='amount']").value;
 
-//from
-const parentEl = document.querySelector("#currency-box-from");
-const fromInputName = "currency_from";
-createCurrencyElements(currencyKeys, parentEl, fromInputName);
+    const currentCurrencyObject = data[fromTarget];
+    const resultForOne = currentCurrencyObject[toTarget];
+    const result = amount * resultForOne;
 
-// to
-const parentToEl = document.querySelector("#currency-box-to");
-const toInputName = "currency_to";
-createCurrencyElements(currencyKeys, parentToEl, toInputName);
+    const currencyResult = document.querySelector("#currency-result");
+    if (fromTarget == toTarget) {
+      currencyResult.innerHTML = "FARKLI DEGERLER SECMELISINIZ!";
 
+    }else if (isNaN(amount)){
+      currencyResult.innerHTML = "DEGERI BIR SAYI OLARAK VERMELISINIZ!";
+    }else {
+      currencyResult.innerHTML = amount + " " + fromTarget + " = " + result + " " + toTarget;
+    }
+  
+    
+   }else if (document.querySelector("input[name='currency_from']:checked")){    
+    currencyResult.innerHTML = "DONUSTURMEK ISTEDIGINIZ DEGERI GIRINIZ!";
 
-const calculateButton = document.querySelector("#calculate-button");
-calculateButton.addEventListener("click", function(){
-   // kimden ceviriyourz
-   const fromTarget = document.querySelector("input[name='currency_from']:checked").value;
-   // kime ceviriyoruz
-   const toTarget   = document.querySelector("input[name='currency_to']:checked").value;
-   // amountu alalim
-   const amount     = document.querySelector("input[name='amount']").value;
+   }else if (document.querySelector("input[name='currency_to']:checked")) {
+    currencyResult.innerHTML = "DONUSTURULECEK DEGER GIRINIZ!";
 
-   const currentCurrencyObject = data[fromTarget];
-   const resultForOne = currentCurrencyObject[toTarget];
-   const result = amount * resultForOne;
+   }else {
+    currencyResult.innerHTML = "HICBIR SECIM YAPMADINIZ SECIM YAPINIZ!";
+   }
 
-   const currencyResult = document.querySelector("#currency-result");
-   currencyResult.innerHTML = amount + " " + fromTarget + " = " + result + " " + toTarget;
-});
+  });
